cb38127990032c17dae69a242672164fa345522b
Francocean <francophoneadeleye@gmail.com>
2020-04-28 12:20:58 -0800

    Updates

---
+++ precache-manifest.0b7570bb1e7256b02d8fce93d9385a40.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d12c47eecb549eed60a5eeaf23cd3ef6",
+    "url": "/react-portfolio-template-2020/index.html"
+  },
+  {
+    "revision": "a8267f4dd3a34cff3198",
+    "url": "/react-portfolio-template-2020/static/css/main.278284ca.chunk.css"
+  },
+  {
+    "revision": "9f924ba328339376cf09",
+    "url": "/react-portfolio-template-2020/static/js/2.fc406b69.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio-template-2020/static/js/2.fc406b69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8267f4dd3a34cff3198",
+    "url": "/react-portfolio-template-2020/static/js/main.ae99a191.chunk.js"
+  },
+  {
+    "revision": "c960a94b279fdf511594",
+    "url": "/react-portfolio-template-2020/static/js/runtime-main.47284ddf.js"
+  },
+  {
+    "revision": "e1906936835c0b3df444ebda3c5738bb",
+    "url": "/react-portfolio-template-2020/static/media/YourResume.e1906936.pdf"
+  },
+  {
+    "revision": "00051dbb5b9c7a7f4dfdf04f1b633445",
+    "url": "/react-portfolio-template-2020/static/media/background.00051dbb.jpg"
+  },
+  {
+    "revision": "ef93b85f91bed41bc96b6a7b7369126c",
+    "url": "/react-portfolio-template-2020/static/media/portrait.ef93b85f.jpg"
+  },
+  {
+    "revision": "31ad0b4d88c6a8f6d6812046509cfd67",
+    "url": "/react-portfolio-template-2020/static/media/portrait_small.31ad0b4d.jpg"
+  },
+  {
+    "revision": "2efd960d6f072c1970115149dc9607d6",
+    "url": "/react-portfolio-template-2020/static/media/project1.2efd960d.jpg"
+  },
+  {
+    "revision": "473d9f55d2f5045e41e3271c338cdb11",
+    "url": "/react-portfolio-template-2020/static/media/project2.473d9f55.jpg"
+  },
+  {
+    "revision": "139308d797d4a548920ce783be94237f",
+    "url": "/react-portfolio-template-2020/static/media/project3.139308d7.jpg"
+  },
+  {
+    "revision": "5a100b46aa5dcadfabc907a76cf78c87",
+    "url": "/react-portfolio-template-2020/static/media/project4.5a100b46.jpg"
+  },
+  {
+    "revision": "babc9580b3b97699fb8a21b526912d87",
+    "url": "/react-portfolio-template-2020/static/media/project5.babc9580.jpg"
+  }
+]);
